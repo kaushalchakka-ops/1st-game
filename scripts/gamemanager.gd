24d7d2a6f37a7ced3de469b5f53be7d3b16719a7
Kaushal Chakka <kaushal_chakka@srmap.edu.in>
2026-01-13 14:12:58 +0530

    commited

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,8 +1,6 @@
 extends Node2D
-
-@onready var gamemanager: Node2D = $"."
-@onready var label: Label = $"../Label"
-
+@onready var gamemanager: Node2D = %gamemanager
+@onready var label: Label = $"../labels/Label"
 var score =0
 func add_point():
 	score+=1
