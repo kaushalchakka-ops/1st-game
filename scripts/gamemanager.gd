a8c489a2afc0c5b9848841b1d2fbb0f6d27f28f8
Kaushal Chakka <kaushal_chakka@srmap.edu.in>
2026-01-13 16:49:11 +0530

    commited

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,8 +1,25 @@
 extends Node2D
 @onready var gamemanager: Node2D = %gamemanager
 @onready var label: Label = $"../labels/Label"
+@onready var glass: AnimatableBody2D = $"../Node/glass"
+
 var score =0
+var invipotion=0
+func visi():
+	invipotion+=1
+	print(invipotion)
+	if invipotion > 0 :
+		glass.visible=true
+
+	
 func add_point():
 	score+=1
 	print(score)
 	label.text = "You collected " + str(score) + " coins."
+
+
+
+
+
+func _on_glass_visibility_changed() -> void:
+	pass # Replace with function body.
---
+++ scripts/potion.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var gamemanager: Node2D = %gamemanager
+
+
+func _on_body_entered(body: Node2D) -> void:
+	gamemanager.visi()
+	queue_free()
